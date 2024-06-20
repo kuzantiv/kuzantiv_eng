fa3546afe1b52ca5df5527cd51635744efed6035
savelevDV <savelevdv@competentit.ru>
2024-06-20 12:57:44 +0600

    проверка правильности слов
    разработан тестовый функционал, для проверки правильно ли было выбрано слово

--- words/views.py
+++ words/views.py
@@ -1,3 +1,4 @@
+import json
 import random
 
 from django.contrib.auth.decorators import login_required
@@ -6,24 +7,45 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .forms import WordForm
 from .models import Word, LearnedWord
 
+context = {
+    'word': "",
+    'translations': [],
+    'chosen_word': None,
+    'status': None
+}
 
 def home(request):
     return render(request, 'words/home.html')
 
 
 def practice_words(request):
+    global context
     words = Word.objects.all()
-    if words.exists():
-        word = random.choice(words)
-        translations = list(words.exclude(id=word.id).values_list('translation', flat=True))
-        translations = random.sample(translations, min(3, len(translations)))  # Ensure we get 3 wrong translations
-        translations.append(word.translation)
-        random.shuffle(translations)
-        context = {
-            'word': word,
-            'translations': translations
-        }
-        return render(request, 'words/practice_words.html', context)
+
+    if request.method == "POST":
+        control_word, chosen_word = request.POST.get("translation").split()
+        word_obj = Word.objects.filter(translation=chosen_word)
+        context['chosen_word'] = chosen_word
+        if str(word_obj[0]) == control_word:
+            context['status'] = True
+            return render(request, 'words/practice_words.html', context)
+        else:
+            context['status'] = False
+            return render(request, 'words/practice_words.html', context)
+
+    if request.method == "GET":
+        if words.exists():
+            word = random.choice(words)
+            translations = list(words.exclude(id=word.id).values_list('translation', flat=True))
+            translations = random.sample(translations, min(3, len(translations)))  # Ensure we get 3 wrong translations
+            translations.append(word.translation)
+            random.shuffle(translations)
+            context['status'] = None
+            context['chosen_word'] = None
+            context['word'] = word
+            context['translations'] = translations
+            return render(request, 'words/practice_words.html', context)
+
     else:
         return render(request, 'words/no_words.html')  # Handle case with no words
 
