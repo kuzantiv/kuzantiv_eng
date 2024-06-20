86031c170cd3ebdb5263915c51bc66b8552fc7df
Anton Kuznetsov <kuzantiv1992@mail.ru>
2024-06-20 14:56:18 +0600

    отлавливаю ошибки

--- build_files.sh
+++ build_files.sh
@@ -7,6 +7,12 @@ then
     exit 1
 fi
 
+# create a virtual environment named 'venv' if it doesn't already exist
+python3.12 -m venv venv
+
+# activate the virtual environment
+source venv/bin/activate
+
 if ! command -v pip &> /dev/null
 then
     echo "pip could not be found"
